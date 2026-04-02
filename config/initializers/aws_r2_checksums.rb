bf106ce0c8e97c4a6f6049f6a64b962cf0003c46
Ilmeori Admin <admin@ilmeori.com>
2026-04-02 22:37:22 +0900

    Fix: 빌드 에러 해결 - Aws 상수 undefined 체크 추가

--- config/initializers/aws_r2_checksums.rb
+++ config/initializers/aws_r2_checksums.rb
@@ -1,3 +1,6 @@
 # Cloudflare R2는 AWS S3와 달리 다중 체크섬 헤더를 지원하지 않습니다.
 # AWS SDK가 자동으로 CRC32 체크섬을 추가하는 것을 비활성화합니다.
-Aws.config.update(s3: { compute_checksums: false })
+# 빌드 시 aws-sdk가 로드되지 않을 수 있으므로 안전하게 처리합니다.
+if defined?(Aws)
+  Aws.config.update(s3: { compute_checksums: false })
+end
