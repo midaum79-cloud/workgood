167772275ff9f674867fae549c71cd2422898934
Ilmeori Admin <admin@ilmeori.com>
2026-04-18 07:59:45 +0900

    fix: refactor MyAccountController to resolve RuboCop styles and Brakeman security warnings

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -2,36 +2,44 @@ class MyAccountController < ApplicationController
   before_action :require_login
   skip_before_action :verify_authenticity_token, only: [:cache_image_for_download]
 
-
   def show
     @project_count   = current_user.projects.count
     @plan_limit      = current_user.plan_limit
   end
+
   def documents
   end
 
   def update_documents
-    user_params = params.fetch(:user, {}).permit(:business_card, :business_registration, :bankbook_copy, :business_bankbook_copy, :bank_name, :bank_account_number, :bank_account_holder, :team_name, :role, :name, :phone, :address)
+    user_params = params.fetch(:user, {}).permit(
+      :business_card, :business_registration,
+      :bankbook_copy, :business_bankbook_copy,
+      :bank_name, :bank_account_number, :bank_account_holder,
+      :team_name, :role, :name, :phone, :address
+    )
 
-    current_user.update(business_card_b64: Base64.strict_encode64(user_params[:business_card].read)) if user_params[:business_card].present?
-    current_user.update(business_registration_b64: Base64.strict_encode64(user_params[:business_registration].read)) if user_params[:business_registration].present?
-    current_user.update(bankbook_copy_b64: Base64.strict_encode64(user_params[:bankbook_copy].read)) if user_params[:bankbook_copy].present?
-    current_user.update(business_bankbook_copy_b64: Base64.strict_encode64(user_params[:business_bankbook_copy].read)) if user_params[:business_bankbook_copy].present?
+    update_file_attribute(:business_card_b64, user_params[:business_card])
+    update_file_attribute(:business_registration_b64, user_params[:business_registration])
+    update_file_attribute(:bankbook_copy_b64, user_params[:bankbook_copy])
+    update_file_attribute(:business_bankbook_copy_b64, user_params[:business_bankbook_copy])
 
     if params[:business_card_base64_direct].present?
       current_user.update(business_card_b64: params[:business_card_base64_direct].split(',').last)
     end
+    
     if params[:bankbook_copy_base64_direct].present?
       current_user.update(bankbook_copy_b64: params[:bankbook_copy_base64_direct].split(',').last)
     end
 
-    text_attrs = user_params.except(:business_card, :business_registration, :bankbook_copy, :business_bankbook_copy).to_h
+    text_attrs = user_params.except(
+      :business_card, :business_registration, :bankbook_copy, :business_bankbook_copy
+    ).to_h
     current_user.update(text_attrs) if text_attrs.present?
 
     current_user.regenerate_document_share_token if current_user.document_share_token.blank?
 
     respond_to do |format|
-      format.html { redirect_to my_account_documents_path, notice: "비즈니스 문서를 성공적으로 업데이트했습니다." }
+      format.html { redirect_to my_account_documents_path, notice: '비즈니스 문서를 성공적으로 업데이트했습니다.' }
       format.json { head :ok }
     end
   end
@@ -40,9 +48,9 @@ class MyAccountController < ApplicationController
     type = params[:type]
     if %w[business_card business_registration bankbook_copy business_bankbook_copy].include?(type)
       current_user.update("#{type}_b64" => nil)
-      redirect_to my_account_documents_path, notice: "해당 서류가 삭제되었습니다."
+      redirect_to my_account_documents_path, notice: '해당 서류가 삭제되었습니다.'
     else
-      redirect_to my_account_documents_path, alert: "잘못된 요청입니다."
+      redirect_to my_account_documents_path, alert: '잘못된 요청입니다.'
     end
   end
 
@@ -69,6 +77,7 @@ class MyAccountController < ApplicationController
       render json: { success: false, limit_reached: true }
     end
   end
+
   def cache_image_for_download
     uuid = SecureRandom.uuid
     base64_data = params[:image_base64]
@@ -81,12 +90,25 @@ class MyAccountController < ApplicationController
   end
 
   def download_cached_image
-    base64_data = Rails.cache.read("image_download_#{params[:uuid]}")
+    safe_uuid = params[:uuid].to_s.gsub(/[^a-zA-Z0-9-]/, '')
+    base64_data = Rails.cache.read("image_download_#{safe_uuid}")
+    
     if base64_data.present?
       image_data = Base64.decode64(base64_data.split(',').last)
-      send_data image_data, type: 'image/png', disposition: 'inline', filename: "일잘러_이미지_#{params[:uuid][0..5]}.png"
+      send_data image_data,
+                type: 'image/png',
+                disposition: 'inline',
+                filename: "일잘러_이미지_#{safe_uuid[0..5]}.png"
     else
       render plain: "이미지 링크가 만료되었거나 찾을 수 없습니다. (10분 한정)\n앱으로 돌아가 다시 시도해주세요.", status: 404
     end
+  end
+
+  private
+
+  def update_file_attribute(attribute, file_param)
+    return unless file_param.present?
+
+    current_user.update(attribute => Base64.strict_encode64(file_param.read))
   end
 end
